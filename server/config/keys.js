69deb2179491cb2fca869c006194eb1fda85b75c
lukepvb <50924085+lukepvb@users.noreply.github.com>
2020-04-03 08:10:20 -0700

    Revert "Models"

--- server/config/keys.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-// this dotenv allows us to hide our mongoURI and access it from the .env file
-dotenv.config();
-const mongoConnect = process.env.MONGO_URI;
-
-module.exports = {
-  mongoURI: mongoConnect
-};
