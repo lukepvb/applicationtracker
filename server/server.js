154d094ef3ade405f61b907012f3e9bf9e9c1dd4
Jason Jones <jasonroyjones@gmail.com>
2020-04-05 17:53:33 -0700

    server boilerplate and initial route handlers

--- server/server.js
+++ server/server.js
@@ -1,30 +1,50 @@
+const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const users = require;
 
 // initialize express
 const app = express();
+
+// create a PORT variable for server listener
+const PORT = process.env.PORT || 3000;
+
+// require routers
+const router = require('./routes/api');
+const userRouter = require('./routes/users');
+const appRouter = require('./routes/apps');
 
 // initialize body-parser to JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// bring in the database key and connect w/ mongoose
-const db = require('./config/keys').mongoURI;
-
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch((err) => console.log('Error connecting to the database', err));
-
-// create a PORT variable to listen to the server
-const PORT = process.env.PORT || 3000;
-
 // handle requests for static files
 app.use('/assets', express.static(path.resolve(__dirname, '../client/assets')));
 
 // define route handlers
-app.use('/api/users', users);
+app.use('/api/user', userRouter);
+app.use('/api/app', appRouter);
+app.use('/api', router);
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+// respond with main app
+app.get('/', (req, res) =>
+  res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'))
+);
+
+// catch-all route handler for any unknown route
+app.use('*', (req, res) => res.status(404).send('Page Not Found'));
+
+// express global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
+module.exports = app;
