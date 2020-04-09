6efad1d551e18af0391b7ebc8dce098615c5b299
Jason Jones <jasonroyjones@gmail.com>
2020-04-09 13:46:55 -0700

    augmented App.jsx to test React Router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import '../assets/styles.css';
 import Login from './Login.jsx';
 import SignUp from './SignUp.jsx';
 
 const App = (props) => {
   return (
-    <div className="router">
-      <main>
-        {/* <Switch> */}
-        <p>Create Login and link to SignUp component from initial page load</p>
+    <Router>
+      <div className="router">
         <Login />
-        <SignUp />
-        {/* <Route exact path="/" component={Login} />
-          <Route exact path="/signup" component={SignUp} /> */}
-        {/* </Switch> */}
-      </main>
-    </div>
+        <Link to="/signup">Don't have an account? Sign up!</Link>
+
+        <Switch>
+          {/* <Route exact path="/" component={Login} /> */}
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
