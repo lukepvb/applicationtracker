d1e065890ed7c80365bf85cb1f4cd3399dccf2fa
Jason Jones <jasonroyjones@gmail.com>
2020-04-09 14:02:40 -0700

    Added reactstrap modal to initial SignUp component and installed bootstrap dependency

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,5 +1,50 @@
-import * as React from 'react';
+/* eslint-disable react/button-has-type */
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 
-const SignUp = (props) => <p>This is the SignUp component</p>;
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const SignUp = (props) => {
+  const { buttonLabel, className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const closeBtn = (
+    <button className="close" onClick={toggle}>
+      &times;
+    </button>
+  );
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>
+        {buttonLabel}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle} close={closeBtn}>
+          Modal title
+        </ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Do Something
+          </Button>{' '}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
 
 export default SignUp;
