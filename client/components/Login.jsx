bd60775dd7e939684b8fef36b2d5b532c6651c38
Jason Jones <jasonroyjones@gmail.com>
2020-04-13 14:56:50 -0700

    added event handlers to login fields

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import SignUp from './SignUp';
 import { MdMail, MdTrackChanges } from 'react-icons/md';
@@ -7,10 +7,23 @@ import { FiSend } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
 
 const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   let history = useHistory();
 
   const handleClick = () => {
     history.push('/dashboard');
+  };
+
+  const handleEmailChange = (e) => {
+    const updatedEmail = e.target.value;
+    setEmail(updatedEmail);
+  };
+
+  const handlePasswordChange = (e) => {
+    const updatedPassword = e.target.value;
+    setPassword(updatedPassword);
   };
 
   return (
@@ -21,14 +34,28 @@ const Login = (props) => {
           <Label for="exampleEmail" className="mr-sm-2">
             <MdMail className="icon-email" /> Email
           </Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="something@idk.cool"
+            value={email}
+            onChange={handleEmailChange}
+          />
         </FormGroup>
         <br />
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="examplePassword" className="mr-sm-2">
             <FaLock className="icon-password" /> Password
           </Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="********" />
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="********"
+            value={password}
+            onChange={handlePasswordChange}
+          />
         </FormGroup>
         <br />
         <div id="login-button">
